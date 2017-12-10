5f6f64bf6dcbf5bb530e13f93df266bee55b2360
Such Wow <netblock@gmail.com>
2017-12-10 10:38:45 +0100

    Upper bound slider should have its min value >= to lower bound's min value.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,39 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+///////////////////////////////////////////////////////////////////////////////
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+  setup_connections();
 }
 
+///////////////////////////////////////////////////////////////////////////////
 MainWindow::~MainWindow() { delete ui; }
+
+///////////////////////////////////////////////////////////////////////////////
+void MainWindow::on_lowerBoundSlider_changed(int value) {
+  ui->lowerBoundCurVal->setText(QString::number(value));
+
+  /// TODO: get max value from some data controller.
+  update_lower_bound_range(value, 100);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void MainWindow::on_upperBoundSlider_changed(int value) {
+  ui->upperBoundCurVal->setText(QString::number(value));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void MainWindow::setup_connections() {
+  connect(ui->lowerBoundSlider, &QSlider::valueChanged, this,
+          &MainWindow::on_lowerBoundSlider_changed);
+  connect(ui->upperBoundSlider, &QSlider::valueChanged, this,
+          &MainWindow::on_upperBoundSlider_changed);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void MainWindow::update_lower_bound_range(int first, int last) {
+  ui->upperBoundSlider->setRange(first, last);
+  ui->upperBoundMinVal->setText(QString::number(first));
+}
--- mainwindow.h
+++ mainwindow.h
@@ -14,7 +14,14 @@ class MainWindow : public QMainWindow {
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
 
+ public slots:
+  void on_lowerBoundSlider_changed(int value);
+  void on_upperBoundSlider_changed(int value);
+
  private:
+  void setup_connections();
+  void update_lower_bound_range(int first, int last);
+
   Ui::MainWindow *ui;
 };
 
